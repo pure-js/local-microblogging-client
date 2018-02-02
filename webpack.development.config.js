d1ed96c0b5a41c2545556ff6d067c05428d56d12
pure-js <3690368+pure-js@users.noreply.github.com>
2018-02-02 15:56:00 +0300

    modify config

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,6 +1,61 @@
-module.exports = {
-  mode: 'development',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const webpack = require('webpack');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'src/index-template.html',
+  }),
+  new ScriptExtHtmlWebpackPlugin({
+    defaultAttribute: 'defer',
+  }),
+  new webpack.NamedModulesPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+];
+
+const config = {
+  entry: {
+    app: './src/index.jsx',
+  },
+  plugins,
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '.tmp'),
+  },
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: 'dist',
+    hot: true,
   },
 };
+
+module.exports = config;
