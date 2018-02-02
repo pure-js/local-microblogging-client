eb73cf6a93dbeeb2c2a118bda59b0c291ca9fbeb
pure-js <3690368+pure-js@users.noreply.github.com>
2018-02-02 20:32:50 +0300

    refactoring

---
+++ src/mock-posts.js
@@ -0,0 +1,36 @@
+const posts = [
+  {
+    heading: 'Automatic Music Generator Jukedeck Wins Le Web Startup Competition',
+    body: 'As per usual the Le Web startup competition has been showcasing a smorgasbord of Europe’s hottest startups this week, and three stood out for the judges. But out of Easysize, Naturalcycles and Jukedeck, it was Jukedeck which won the top prize in the end.',
+  },
+  {
+    heading: 'Convoy! The Fueloyal Is A Smart Fuel Cap For Truckers',
+    body: 'Breaker breaker 10-4 good buddy. We got Fueloyal on your tail coming up off the exit. This robotic fuel cap essentially keeps track of the fuel poured into your vehicle independently of the truck itself and lets fleet owners ensure they no one is stealing the fuel poured into their 18-wheelers.',
+  },
+  {
+    heading: 'The da Vinci 1.0 AiO Is The Future Of All-In-One 3D Printers',
+    body: 'The AiO is a closed box that contains a full ABS 3D printing system as well as a laser 3D scanner. A turntable under the built platform spins objects slowly as a laser takes in their contours and the resulting objects can be printed directly from the scanning software. It is literally a 3D copier with true object-in/object-out systems. In short, it is a Star Trekian replicator – within reason.',
+  },
+  {
+    heading: 'Microsoft Begins Accepting Bitcoin For Windows, Windows Phone And Xbox Purchases',
+    body: 'If you want further proof that Microsoft is going through a transformative phase, you may be interested to hear that the company has jumped on the bitcoin wagon.',
+  },
+  {
+    heading: 'Workflow Lets You Automate The Stuff You Do All The Time On Your iPhone',
+    body: 'Built for the tastes of power users with an interface that anyone can understand, Workflow is a new app for iOS that lets you create shortcuts for the things you do all the time on your phone or tablet. With a simple, efficient interface, it lets anyone automate things like creating GIFs from the pictures in your photo roll to ordering an Uber going to the address for the next event on your calendar.',
+  },
+  {
+    heading: 'Chromecast Now Lets Your Guests Take Over Your TV Without Needing Your WiFi Password',
+    body: 'Back in June, Google announced a rather nifty new feature coming to Chromecast: your friends and house guests would soon be able to connect to your Chromecast without being on your WiFi network, thanks to the clever use of magic ultrasonic sounds.',
+  },
+  {
+    heading: 'New “Shingled” Hard Drives Hold Terabytes For Pennies A Gig',
+    body: 'While the last time most of us thought of shingles was when we were itchy in eighth grade, Seagate has been thinking of them as a way to store data. Called Shingled Magnetic Recording (SMR) Drives, Seagate’s new drives can store 8 terabytes of data for about 3 cents a gigabyte.',
+  },
+  {
+    heading: 'Swill Launches In New York To Make Buying Alcohol A ‘Seamless’ Experience',
+    body: 'Swill has launched in NYC, offering users an easy way to order booze with their phone and have it delivered on the spot.',
+  },
+];
+
+export default posts;
