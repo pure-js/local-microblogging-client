15fc2e479ea8b6ccb84923173c4c056ac466073e
pure-js <3690368+pure-js@users.noreply.github.com>
2018-02-02 17:39:31 +0300

    add App React component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import StoriesFeed from './StoriesFeed.jsx';
+
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <StoriesFeed />
+    );
+  }
+}
+
+export default App;
