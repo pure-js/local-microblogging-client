2c1f53086495a1c50eec5db8c46dbc6780df7fad
pure-js <3690368+pure-js@users.noreply.github.com>
2018-02-05 12:47:56 +0300

    save input values

--- src/components/CreateStory.jsx
+++ src/components/CreateStory.jsx
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 
 class CreateStory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      heading: '',
+      body: '',
+    };
+
+    this.handleHeadingChange = this.handleHeadingChange.bind(this);
+    this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.createStory = this.createStory.bind(this);
+  }
+
+  handleHeadingChange(event) {
+    this.setState({
+      heading: event.target.value,
+    });
+  }
+
+  handleBodyChange(event) {
+    this.setState({
+      body: event.target.value,
+    });
+  }
+
+  createStory(event) {
+    event.preventDefault();
+    const story = {
+      heading: this.state.heading,
+      body: this.state.body,
+    };
+    alert(this.state.heading);
+  }
+
   render() {
-    function onSubmit() {}
-
-    function readURL(input) {
-      if (input.files && input.files[0]) {
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-          document.getElementById('preview').src = e.target.result;
-        };
-
-        reader.readAsDataURL(input.files[0]);
-      }
-    }
-
     return (
       <div className="card mb-3">
         <img id="preview" src="" alt="Card cap" className="card-img-top" />
         <div className="card-body">
-          <h3 className="card-title">What's new?</h3>
-          <form>
+          <h3 className="card-title">{'What\'s new?'}</h3>
+          <form onSubmit={this.createStory}>
             <div className="form-group">
               <input
                 placeholder="Your heading"
                 type="text"
                 className="form-control js-new-post__heading"
+                value={this.state.heading}
+                onChange={this.handleHeadingChange}
               />
             </div>
             <div className="form-group">
@@ -34,18 +56,19 @@ class CreateStory extends Component {
                 placeholder="Write something"
                 rows="3"
                 className="form-control js-new-post__main-text"
+                value={this.state.body}
+                onChange={this.handleBodyChange}
               />
             </div>
             <input
               type="file"
               title="Add image"
-              onChange={readURL}
               className="btn btn-default new-post__add-image"
             />
             <button
               title="Add new post"
-              type="button"
-              onClick={onSubmit}
+              type="submit"
+              // onSubmit={this.createStory}
               className="btn btn-primary btn-lg new-post__submit"
             >
               <i className="glyphicon glyphicon-ok" />Post it
