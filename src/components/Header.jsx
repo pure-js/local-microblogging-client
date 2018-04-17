95484da3cfef432d8ccafd4ef68dc0e1d89afa16
pure-js <3690368+pure-js@users.noreply.github.com>
2018-04-18 00:46:26 +0300

    header to pre function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import bootstrap from 'bootstrap/scss/bootstrap.scss';
 import PropTypes from 'prop-types';
-import StoriesFeed from './Story';
 
-class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <a className="navbar-brand" href="#home">Local microblogging client</a>
-          <div>
-            <form className="form-inline my-2 my-lg-0">
-              <button
-                className="btn btn-outline-primary my-2 my-sm-0"
-                type="button"
-                onClick={this.props.showForm}
-              >New post
-              </button>
-            </form>
-          </div>
+function Header(props) {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#home">Local microblogging client</a>
+        <div>
+          <form className="form-inline my-2 my-lg-0">
+            <button
+              className="btn btn-outline-primary my-2 my-sm-0"
+              type="button"
+              onClick={props.showForm}
+            >New post
+            </button>
+          </form>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 Header.propTypes = {
