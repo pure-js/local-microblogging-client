1fd62d2472a63e402bd1b33f969fdef8336871ee
Årthur <3690368+pure-js@users.noreply.github.com>
2022-07-07 03:05:08 +0400

    feat: add blog post page (#37)
    * chore: remove unused file
    
    * feat: add blog post page

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,22 +1,41 @@
-import { useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { useLiveQuery } from 'dexie-react-hooks';
+import { useParams } from 'react-router-dom';
+import { useLiveQuery } from 'dexie-react-hooks';
+
 import type { IBlogPost } from '../components/PostPreview';
+import { db } from '../services/db';
 
-// import { db } from '../services/db';
-
-function BlogPost() {
-  const [post] = useState<IBlogPost | undefined>();
-  // const { postId } = useParams();
-
+function Post({ heading, text }: IBlogPost) {
   return (
     <main className="container">
-      <h1>
-        Heading
-        { post?.heading }
-      </h1>
+      <div className='row'>
+        <article className="col-md-8 col-lg-6">
+          <h1>
+            { heading }
+          </h1>
+          <p>
+          { text }
+          </p>
+        </article>
+      </div>
     </main>
   );
+}
+
+function BlogPost() {
+  const { postId } = useParams();
+  const postArr = useLiveQuery(
+    () => db.posts
+      .filter(({ id }) => id === Number(postId))
+      .toArray()
+  );
+
+  return postArr ? postArr.map(({ id, heading, text }) => (
+    <Post
+      key={id}
+      heading={heading}
+      text={text}
+    />) 
+  ) : null
 }
 
 export default BlogPost;
