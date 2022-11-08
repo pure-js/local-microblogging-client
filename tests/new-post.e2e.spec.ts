99adddaac3051c915247cb4887e28aa6cdc7c562
pure-js <3690368+pure-js@users.noreply.github.com>
2022-11-08 22:23:08 +0300

    chore: remove unnecessary lines of code

--- tests/new-post.e2e.spec.ts
+++ tests/new-post.e2e.spec.ts
@@ -1,9 +1,4 @@
 import { test, expect } from '@playwright/test';
-// import { test, expect, Page } from '@playwright/test';
-
-// async function checkTodosInLocalStorage(page: Page, title: string) {
-//   return page.waitForFunction((t) => JSON.parse(indexedDB['Platform']).map((todo: any) => todo.title).includes(t), title);
-// }
 
 test('test', async ({ page }) => {
   await page.goto('/posts/new');
@@ -13,7 +8,4 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Write your text...').fill('Test body');
   await page.getByRole('button', { name: 'Post it' }).click();
   await expect(page).toHaveURL('/');
-  // await checkTodosInLocalStorage(page, 'buy some sausages');
-  // await page.getByRole('heading').click();
-  // await expect(page).toHaveURL('http://localhost:5173/');
 });
