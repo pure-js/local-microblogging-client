89839167cdaebffd3abe232030f73f81d162c397
pure-js <3690368+pure-js@users.noreply.github.com>
2022-11-08 21:56:07 +0300

    chore: add base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,8 +39,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'https://pure-js.github.io/local-microblogging-client/',
-
+    baseURL: 'http:/localhost:4173/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
