a1a28d37dd95d12b348c1ea1618b04a91fdd57e0
pure-js <3690368+pure-js@users.noreply.github.com>
2022-12-12 01:51:02 +0700

    build: use swr instead of babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // const path = require('node:path');
@@ -10,10 +10,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   plugins: [
-    react({
-      // Use React plugin in all *.jsx and *.tsx files
-      include: 'src/**/*.{jsx,tsx}',
-    }),
+    react(),
     VitePWA({}),
   ],
   define: {
