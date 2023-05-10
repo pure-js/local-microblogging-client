21635c80949c719f210f51e977c3fc4c53189ff0
Årthဝur <3690368+pure-js@users.noreply.github.com>
2023-05-11 03:01:00 +0800

    fix: unit test (#170)
    * refactor: rewrite to pure function
    
    * build(deps-dev): bump jest & ts-jest deps
    
    * chore: add coverage reports

--- src/services/timestampToLocaleString.test.ts
+++ src/services/timestampToLocaleString.test.ts
@@ -4,9 +4,11 @@
 import { timestampToLocaleString } from './timestampToLocaleString';
 
 test('1654024577 equals May 31', () => {
-  expect(timestampToLocaleString(1654024577)).toStrictEqual({ date: 'May 31', htmlDatetime: '2022-05-31' });
+  expect(timestampToLocaleString(1654024577, new Date('2022-06-12')))
+    .toStrictEqual({ date: 'May 31', htmlDatetime: '2022-05-31' });
 });
 
 test('1619792177 equals May 31', () => {
-  expect(timestampToLocaleString(1619792177)).toStrictEqual({ date: 'April 30, 2021', htmlDatetime: '2021-04-30' });
+  expect(timestampToLocaleString(1619792177))
+    .toStrictEqual({ date: 'April 30, 2021', htmlDatetime: '2021-04-30' });
 });
