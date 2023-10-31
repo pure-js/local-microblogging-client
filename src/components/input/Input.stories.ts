e28bfaecbfcd720c1beeb529fa6060d54a2136d0
Arthur Green <curious.dev@icloud.com>
2023-10-31 10:29:39 +0400

    chore: force case sensetivity

---
+++ src/components/input/Input.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from './Input';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Components/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+  },
+};
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,49 @@
+interface InputProps {
+  /**
+   * How large should the button be?
+   */
+  size?: 'medium' | 'large';
+  /**
+   * What placeholder color to use
+   */
+  placeholder?: string;
+  /**
+   * Button contents
+   */
+  label?: string;
+  /**
+   * Input value
+   */
+  value?: string;
+  /**
+   * Optional change handler
+   */
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function Input({
+  size = 'large',
+  placeholder,
+  value,
+  onChange,
+  label,
+  ...props
+}: InputProps) {
+  const sizeAdjusted = size === 'large' ? '3xl' : 'xl';
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className={['input', 'w-full', `text-${sizeAdjusted}`].join(' ')}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
+  );
+}
+
+export default Input;
