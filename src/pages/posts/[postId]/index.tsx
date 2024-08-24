e1d7af3346ba0fda0d9a17e9c8e0305dc890d89f
Arthur Green <curious.dev@icloud.com>
2024-08-24 16:53:08 +0400

    chore: change classname order

--- src/pages/posts/[postId]/index.tsx
+++ src/pages/posts/[postId]/index.tsx
@@ -15,11 +15,11 @@ function Post({ heading, text }: IBlogPost) {
     <main className="container mx-auto px-4">
       <div className="grid grid-cols-12 gap-1">
         <article className="col-span-12 md:col-span-8 lg:col-span-6">
-          <h1 className="text-4xl mt-2 mb-6">{heading}</h1>
+          <h1 className="mb-6 mt-2 text-4xl">{heading}</h1>
           <p className="text-lg">{text}</p>
           <button
             type="button"
-            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-5"
+            className="my-5 rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white"
             onClick={handleNavigate}
           >
             Go to home
